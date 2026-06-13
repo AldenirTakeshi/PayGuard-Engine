8e3cfdd721bf44c9571bde335ae66340c416918d
Aldenir dos Santos <89494499+AldenirTakeshi@users.noreply.github.com>
2026-06-13 11:20:44 -0400

    feat: inicializa estrutura do monorepo com maven multi-module (US-01)
    - Criados os módulos payguard-core-api, payguard-anti-fraud e payguard-notification.
    - Configurado o pom.xml raiz para gerenciamento centralizado de dependências e do Spring Boot 3.2.4.
    - Criadas as classes principais de inicialização do Spring Boot nos pacotes corretos de cada microsserviço.

---
+++ payguard-anti-fraud/src/main/java/com/payguard/antifraud/AntiFraudApplication.java
@@ -0,0 +1,11 @@
+package com.payguard.antifraud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AntiFraudApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(AntiFraudApplication.class, args);
+    }
+}
